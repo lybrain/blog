7d725baae646bfdc34f1883e1b61674f7fd227b5
anton <www.librain@gmail.com>
2021-11-29 17:42:25 +0600

    db settings secured

--- blog/settings.py
+++ blog/settings.py
@@ -85,7 +85,16 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 try:
-    from blog.db_settings import DATABASES1
+    # from blog.db_settings import DATABASES as db
+    # DATABASES = db
+    DATABASES = {'default': {
+                             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                             'NAME': os.environ['DB_NAME'],
+                             'USER': os.environ['DB_USER'],
+                             'PASSWORD': os.environ['DB_PASSWORD'],
+                             'HOST': os.environ['DB_HOST'],
+                             'PORT': os.environ['DB_PORT']
+                             }}
 except ImportError:
     print('****Database settings error using default****')
     DATABASES = {'default': {
