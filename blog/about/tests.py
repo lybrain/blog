2974c5f5b3bff8e05b618f95edc4d7d28dabdbf7
anton <www.librain@gmail.com>
2021-12-22 16:44:46 +0600

    wish msg edit

--- blog/about/tests.py
+++ blog/about/tests.py
@@ -73,7 +73,7 @@ class UpdateWishMessageTest(BaseWishMessageTest):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
      
     def test_can_partial_update_wish_message(self):
-        response = self.client.put(reverse('wish-message-detail', args=[self.id]), self.partial_data)
+        response = self.client.patch(reverse('wish-message-detail', args=[self.id]), self.partial_data)
         self.check_objs_equality(self.partial_data, response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
